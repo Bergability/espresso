d29ad69bdfdeacb6fe1059076b1b07133ef0431b
Berg <berg@Bergs-MBP.attlocal.net>
2021-04-13 18:30:37 -0500

    Fresh start

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const domContainer = document.getElementById("mount");
+ReactDOM.render(<p>Hello test</p>, domContainer);
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = [
+  {
+    mode: "development",
+    entry: "./src/main.ts",
+    target: "electron-main",
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          include: /src/,
+          use: [{ loader: "ts-loader" }],
+        },
+      ],
+    },
+    output: {
+      path: __dirname + "/dist",
+      filename: "main.js",
+    },
+  },
+  {
+    mode: "development",
+    entry: "./src/index.tsx",
+    target: "electron-renderer",
+    devtool: "source-map",
+    module: {
+      rules: [
+        {
+          test: /\.ts(x?)$/,
+          include: /src/,
+          use: [{ loader: "ts-loader" }],
+        },
+      ],
+    },
+    output: {
+      path: __dirname + "/dist",
+      filename: "app.js",
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+      }),
+    ],
+  },
+];
