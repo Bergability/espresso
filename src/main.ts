60da469a3a5b21cd2b24632a166d5c69baaa5736
bergability <berg@bergability.com>
2021-05-27 21:02:11 -0500

    Add auto updater logic

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,35 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, autoUpdater, dialog } from 'electron';
+import * as isDev from 'electron-is-dev';
 import * as path from 'path';
+
+const server = 'https://releases.espresso.gg';
+const url = `${server}/update/${process.platform}/0.0.2`;
+
+if (!isDev) {
+    autoUpdater.setFeedURL({ url });
+    setInterval(() => {
+        autoUpdater.checkForUpdates();
+    }, 30 * 60 * 1000);
+
+    autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+        const dialogOpts = {
+            type: 'info',
+            buttons: ['Restart', 'Later'],
+            title: 'Application Update',
+            message: process.platform === 'win32' ? releaseNotes : releaseName,
+            detail: 'A new version has been downloaded. Restart the application to apply the updates.',
+        };
+
+        dialog.showMessageBox(dialogOpts).then((returnValue) => {
+            if (returnValue.response === 0) autoUpdater.quitAndInstall();
+        });
+    });
+
+    autoUpdater.on('error', (message) => {
+        console.error('There was a problem updating the application');
+        console.error(message);
+    });
+}
 
 import './core/espresso';
 import './registrations';
